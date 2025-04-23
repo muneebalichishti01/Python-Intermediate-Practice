6c1d57199f2cf50cdf70a0351b2bf305a941ea2d
Muneeb Ali <muneeb.ali1@ucalgary.ca>
2025-04-23 12:37:24 -0600

    refactor(question_model) change method description

--- Quiz_Game_Start_OOP/question_model.py
+++ Quiz_Game_Start_OOP/question_model.py
@@ -1,5 +1,5 @@
 class Question:
-    '''Custom class for user object'''
+    '''Custom class for question object'''
     def __init__(self, q_text: str, q_answer: str) -> None:
         '''Constructor method to initialise attributes'''
         self.text = q_text
